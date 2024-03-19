b84ccea4357cc4a69c46eea1b0eeb774b7ccf94e
BlushedNanis <luis_danieltg@outlook.com>
2024-03-19 13:31:05 -0400

    Add functionality to the script, and add comments

--- email_function.py
+++ email_function.py
@@ -6,7 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def send_email(email_body):
+def send_email(email_body:str):
+    """
+    Send an Email using a SMTP_SSL instance
+    with the given enviromental variables for the sender
+    and reciever email.
+
+    Args:
+        email_body (str): Message for the email, 
+        in case of special characters it must be encoded in utf-8
+    """
     with SMTP_SSL("smtp.gmail.com", 
                   context=create_default_context()) as server:
         server.login(getenv("email"), getenv("pypass"))
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from requests import request
+from email_function import send_email
+from requests import get
+from os import getenv
+from dotenv import load_dotenv
 
 
+#Load envioremental variables from .env
+load_dotenv()
+
+#Make a request
+request = get(getenv("url"))
+
+#Obtain a dictionary from the request
+content = request.json()
+
+#Set Subject for the email
+message = "Subject: Today's News\n"
+
+#Extract the information from the dictionary 
+#and add it to the message
+for article in content["articles"]:
+    message += f"""
+    {article["author"]} - {str(article["publishedAt"][:10])}
+    {article["description"]}
+    {article["title"]}
+    {article["url"]}"""
+
+send_email(message.encode())
